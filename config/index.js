d10df0bfa446e064fca5f3df8c0f86108a006b88
yydsllqase <114017292+yydsllqase@users.noreply.github.com>
2022-09-23 00:38:14 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx2d1c7ce4f5393a4d",
+  APP_ID: "",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "8770fe07e8a9367fff5cd187d7ad55f2",
+  APP_SECRET: "",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -60,15 +60,15 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "lixiang",
+      name: "",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ozEnL55z5Cc1x0jclOqRUuukt2mk",
+      id: "",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "wvF8TeeNC3fvPiJdN4h4pcSTa0aS9ABy-HokHMkJez0",
+      useTemplateId: "",
       // 所在省份
-      province: "湖南",
+      province: "",
       // 所在城市
-      city: "衡阳",
+      city: "",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
